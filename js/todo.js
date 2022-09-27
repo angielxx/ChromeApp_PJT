6130990b1fa939c5ea00aa7e25c334b546b8e98c
angielxx <angielxx94@gmail.com>
2022-09-27 17:52:14 +0900

    work in progress 0927

--- js/todo.js
+++ js/todo.js
@@ -1,25 +1,42 @@
+
+// tag form 관련
+const addTagButton = document.querySelector('#plus-button')
+const tagForm = document.querySelector('#addForm-tag')
+const closeTagButton = document.querySelector('#addForm-tag .close-row img')
+const tagInput = document.querySelector('#addForm-tag input')
+const tagList = document.querySelector('#tag-list #select')
+
+// todo form 관련
 const addButton = document.querySelector('#add-button')
-
-const closeButton = document.querySelector('.close-row img')
-
 const todoForm = document.querySelector('.addForm')
+const closeButton = document.querySelector('.addForm .close-row img')
 const todoInput = document.querySelector('.addForm input')
 const todoList = document.querySelector('#todo-list')
 
+// local storage key
 const TODOS_KEY = 'todos';
 const TAGS_KEY = 'tags';
 
+// local storage에 저장할 객체들
 let toDos = [];
+let tags = [];
 
+///////////////////// start : todoForm ///////////////////////
+// toDos 객체 저장
+function savedTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+// todo 삭제
 function deleteTodo(event) {
     const check = event.currentTarget.parentNode
     console.log(check)
     check.remove()
     toDos = toDos.filter((todo) => todo.id !== parseInt(check.id))
     savedTodos()
-
 }
 
+// todo status에 따라 체크 하기
 function checkTodo(event) {
     let checkImg = event.target
     let check = event.currentTarget.parentNode.parentNode
@@ -28,47 +45,43 @@ function checkTodo(event) {
     let span = checkImg.parentNode.querySelector('span')
     if (todo.status === 'unchecked') {
         todo.status = 'checked'
-        checkImg.src = "./img/icon/check1.png"
+        checkImg.src = "./img/icon/check2.png"
         span.classList.add('checked')
     } else {
         todo.status = 'unchecked'   
-        checkImg.src = "./img/icon/check2.png"
+        checkImg.src = "./img/icon/check1.png"
         span.classList.remove('checked')
     }
     savedTodos()
 }
 
-function savedTodos() {
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
-}
-
+// todo 보여주기
 function showlist(newTodo) {
     const outerDiv = document.createElement('div')
     outerDiv.classList.add('box')
     outerDiv.classList.add('todo')
     outerDiv.id = newTodo.id
-
+    
     const innerDiv = document.createElement('div')
     innerDiv.classList.add('head')
-
+    
     const span = document.createElement('span')
     span.innerText = newTodo.text
-
+    
     const checkImg = document.createElement('img')
     if (newTodo.status === 'unchecked') {
-        checkImg.src = "./img/icon/check2.png"
-        span.classList.remove('checked')
-    } else { 
         checkImg.src = "./img/icon/check1.png"
+        span.classList.remove('checked')
+    } else {
+        checkImg.src = "./img/icon/check2.png"
         span.classList.add('checked')
     }
     checkImg.addEventListener('click', checkTodo)
-
-
+    
     const trashImg = document.createElement('img')
     trashImg.src = './img/icon/trash.png'
     trashImg.addEventListener('click', deleteTodo)
-
+    
     outerDiv.appendChild(innerDiv)
     outerDiv.appendChild(trashImg)
     innerDiv.appendChild(checkImg)
@@ -76,16 +89,21 @@ function showlist(newTodo) {
     todoList.appendChild(outerDiv)
 }
 
+// todoForm 열기
 function onAddClick(event) {
     event.preventDefault()
     todoForm.classList.remove('hidden')
 }
 
+//  todoForm 닫기
 function onCloseClick(event) {
     event.preventDefault()
     todoForm.classList.add('hidden')
+    tagForm.classList.add('hidden')
 }
 
+
+// todoForm 제출
 function onTodoSubmit(event) {
     event.preventDefault()
     const newTodo = todoInput.value
@@ -99,11 +117,62 @@ function onTodoSubmit(event) {
     toDos.push(todoObj)
     showlist(todoObj)
     savedTodos()
-
+    
     todoForm.classList.add('hidden')
+    tagForm.classList.add('hidden')
+}
+///////////////////// end : todoForm ///////////////////////
+
+
+///////////////////// start : tagForm ///////////////////////
+// tag 보여주기
+function showTags(newTag) {
+    const option = document.createElement('option')
+    option.classList.add('tag')
+    option.id = newTag.id
+    option.value = newTag.text
+    option.innerText = newTag.text
+    console.log(option)
+    tagList.appendChild(option)
 }
 
+// tagForm 열기
+function onAddTagClick(event) {
+    event.preventDefault()
+    tagForm.classList.remove('hidden')
+}
+
+// tagForm 닫기
+function onTagCloseClick(event) {
+    event.preventDefault()
+    tagForm.classList.add('hidden')
+}
+
+// tags 객체 저장
+function saveTags() {
+    localStorage.setItem(TAGS_KEY, JSON.stringify(tags))
+}
+
+// tagForm 제출
+function onTagSubmit(event) {
+    event.preventDefault()
+    const newTag = tagInput.value
+    tagInput.value = ''
+    const tagObj = {
+        text: newTag,
+        id: Date.now()
+    }
+    tags.push(tagObj)
+    showTags(tagObj)
+    console.log('give me my tag!')
+    saveTags()
+
+    tagForm.classList.add('hidden')
+}
+///////////////////// end : tagForm ///////////////////////
+
 const savedTodo = localStorage.getItem(TODOS_KEY)
+const savedTags = localStorage.getItem(TAGS_KEY)
 
 if (savedTodo !== null) {
     const parsedTodo = JSON.parse(savedTodo)
@@ -111,6 +180,22 @@ if (savedTodo !== null) {
     parsedTodo.forEach(showlist)
 }
 
+if (savedTags !== null) {
+    const parsedTags = JSON.parse(savedTags)
+    tags = parsedTags
+    parsedTags.forEach(showTags)
+}
+
+// todoForm 열기
 addButton.addEventListener('click', onAddClick)
+// todoForm 닫기
 closeButton.addEventListener('click', onCloseClick)
+// todoForm 제출
 todoForm.addEventListener('submit', onTodoSubmit)
+
+// tagForm 닫기
+closeTagButton.addEventListener('click', onTagCloseClick)
+// tagForm 열기
+addTagButton.addEventListener('click', onAddTagClick)
+// tagForm 제출
+tagForm.addEventListener('submit', onTagSubmit)
